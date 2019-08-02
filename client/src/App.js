05523c744266db36be0749e6d0445c557be5b9a9
Ilia Volkov <ilya.j0b@yandex.ru>
2019-08-02 11:36:36 +0300

    add some routes and global styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Index from "./pages/Index";
+import "./App.scss";
+import Portfolio from "./pages/Portfolio";
+import Project from "./pages/Project";
+import CV from "./pages/CV";
 
-function App() {
-  return <div className="App" />;
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/portfolio/:id" exact component={Project} />
+            <Route path="/portfolio" exact component={Portfolio} />
+            <Route path="/cv" exact component={CV} />
+            <Route path="/" exact component={Index} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
