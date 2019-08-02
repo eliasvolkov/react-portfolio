0d596eccdd5b02b8e6c6a2e88d391f5701c003d0
Ilia Volkov <ilya.j0b@yandex.ru>
2019-08-02 11:30:37 +0300

    add some logic for Card

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./Card.module.scss";
 
-const Card = () => {
+const Card = ({ techologies, links }) => {
   return (
     <div className={styles.card}>
       <div className={styles.links}>
-        <a href="#">
+        <a href={links[0]}>
           <i className="fa fa-desktop" aria-hidden="true" />
           <strong>Live preview</strong>
         </a>
-        <a href="#">
+        <a href={links[1]}>
           <i className="fa fa-code-fork" aria-hidden="true" />
           <strong>Sourse code</strong>
         </a>
@@ -18,10 +18,14 @@ const Card = () => {
       <div className={styles.technologies}>
         <h5>Technologies Used</h5>
         <ul className={styles.list}>
-          <li className={styles.item}>
-            <i className="fa fa-check-circle-o" aria-hidden="true" />
-            {"item"}
-          </li>
+          {techologies.map((item, index) => {
+            return (
+              <li className={styles.item} key={index}>
+                <i className="fa fa-check-circle-o" aria-hidden="true" />
+                {item}
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
